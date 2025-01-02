f4b75ea10fcfe1b9cc2312fb3d95566686d3bb51
Eric Laptop <buaalh@buaa.edu.cn>
2025-01-02 21:17:37 +0800

    试提交子模块代码缩进更改

--- controllers/src/test_controller.cpp
+++ controllers/src/test_controller.cpp
@@ -1,22 +1,22 @@
-#include <pluginlib/class_loader.hpp>
 #include <controller_interface/controller_interface.hpp>
+#include <pluginlib/class_loader.hpp>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  // To avoid unused parameter warnings
-  rclcpp::init(argc, argv);
+    // To avoid unused parameter warnings
+    rclcpp::init(argc, argv);
 
-  pluginlib::ClassLoader<controller_interface::ControllerInterface> loader("controller_interface", "controller_interface::ControllerInterface");
-  std::shared_ptr<controller_interface::ControllerInterface> my_hard;
-  try
-  {
-     my_hard = loader.createSharedInstance("controllers::ForwardController");
-     my_hard->initialize("controller", "");
-     rclcpp::spin(my_hard->get_node()->get_node_base_interface());
-  }
-  catch(pluginlib::PluginlibException& ex)
-  {
-    printf("The plugin failed to load for some reason. Error: %s\n", ex.what());
-  }
-  return 0;
+    pluginlib::ClassLoader<controller_interface::ControllerInterface> loader("controller_interface", "controller_interface::ControllerInterface");
+    std::shared_ptr<controller_interface::ControllerInterface> my_hard;
+    try
+    {
+        my_hard = loader.createSharedInstance("controllers::ForwardController");
+        my_hard->initialize("controller", "");
+        rclcpp::spin(my_hard->get_node()->get_node_base_interface());
+    }
+    catch (pluginlib::PluginlibException &ex)
+    {
+        printf("The plugin failed to load for some reason. Error: %s\n", ex.what());
+    }
+    return 0;
 }
--- hardware_interface/include/hardware_interface/robot_interface.hpp
+++ hardware_interface/include/hardware_interface/robot_interface.hpp
@@ -2,50 +2,50 @@
 #define ROBOT_INTERFACE_HPP
 
 #include "hardware_interface/command_interface.hpp"
-#include "hardware_interface/state_interface.hpp"
-#include "urdf/model.h"
-#include "robot_math/robot_math.hpp"
-#include "rclcpp_lifecycle/lifecycle_node.hpp"
-#include "rclcpp/rclcpp.hpp"
 #include "hardware_interface/hardware_interface.hpp"
+#include "hardware_interface/state_interface.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "robot_math/robot_math.hpp"
+#include "urdf/model.h"
 #include <functional>
 
 namespace hardware_interface
 {
 
-  class RobotInterface : public HardwareInterface
-  {
-  public:
-    ~RobotInterface() {}
-    RobotInterface();
-    int configure_urdf(const std::string &robot_description);
-    void finalize() override;
-    const std::vector<std::string> &get_joint_names() { return joint_names_; }
-    int get_dof() { return dof_; }
-    const urdf::Model &get_urdf_model() { return robot_model_; }
-    const robot_math::Robot &get_robot_model() { return robot_; }
-    std::vector<rclcpp::node_interfaces::NodeBaseInterface::SharedPtr> get_all_nodes();
-    void robot_dynamics(const std::vector<double> &x, std::vector<double> &dx, double t,
-                        std::function<Eigen::MatrixXd (double )> f_external, 
-                        std::function<std::vector<double> (double, const std::vector<double> &, const Eigen::MatrixXd &)> controller);
-    void write(const rclcpp::Time &t, const rclcpp::Duration &period) override; 
-    // for simulation only
-    void write_state(const std::vector<double> &state, const std::vector<double> &force)
+    class RobotInterface : public HardwareInterface
     {
-      std::copy(state.begin(), state.begin() + dof_, state_["position"].begin());
-      std::copy(state.begin() + dof_, state.begin() + 2 * dof_, state_["velocity"].begin());
-      state_["force"] = force;
-    }
+    public:
+        ~RobotInterface() {}
+        RobotInterface();
+        int configure_urdf(const std::string &robot_description);
+        void finalize() override;
+        const std::vector<std::string> &get_joint_names() { return joint_names_; }
+        int get_dof() { return dof_; }
+        const urdf::Model &get_urdf_model() { return robot_model_; }
+        const robot_math::Robot &get_robot_model() { return robot_; }
+        std::vector<rclcpp::node_interfaces::NodeBaseInterface::SharedPtr> get_all_nodes();
+        void robot_dynamics(const std::vector<double> &x, std::vector<double> &dx, double t,
+                            std::function<Eigen::MatrixXd(double)> f_external,
+                            std::function<std::vector<double>(double, const std::vector<double> &, const Eigen::MatrixXd &)> controller);
+        void write(const rclcpp::Time &t, const rclcpp::Duration &period) override;
+        // for simulation only
+        void write_state(const std::vector<double> &state, const std::vector<double> &force)
+        {
+            std::copy(state.begin(), state.begin() + dof_, state_["position"].begin());
+            std::copy(state.begin() + dof_, state.begin() + 2 * dof_, state_["velocity"].begin());
+            state_["force"] = force;
+        }
 
-    CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
+        CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
 
-  protected:
-    std::vector<std::string> joint_names_;
-    int dof_;
-    urdf::Model robot_model_;
-    robot_math::Robot robot_;
-    std::map<std::string, hardware_interface::HardwareInterface::SharedPtr> components;
-  };
+    protected:
+        std::vector<std::string> joint_names_;
+        int dof_;
+        urdf::Model robot_model_;
+        robot_math::Robot robot_;
+        std::map<std::string, hardware_interface::HardwareInterface::SharedPtr> components;
+    };
 
 } // namespace hardware
 
